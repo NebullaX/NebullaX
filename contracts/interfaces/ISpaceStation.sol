37826b73c28a2a216a1f1e56bc5ec968aeae8bb6
NebullaX <94897257+NebullaX@users.noreply.github.com>
2022-08-24 16:47:31 +0800

    Create ISpaceStation.sol

---
+++ contracts/interfaces/ISpaceStation.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.7.6;
+
+import {IStarNFT} from "./IStarNFT.sol";
+
+
+interface ISpaceStation {
+
+    function claim(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256 _dummyId,
+        uint256 _powah,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable;
+
+    function claimBatch(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256[] calldata _dummyIdArr,
+        uint256[] calldata _powahArr,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable;
+
+    function claimCapped(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256 _dummyId,
+        uint256 _powah,
+        uint256 _cap,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable;
+
+    function claimBatchCapped(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256[] calldata _dummyIdArr,
+        uint256[] calldata _powahArr,
+        uint256 _cap,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable;
+
+    function forge(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256[] calldata _nftIDs,
+        uint256 _dummyId,
+        uint256 _powah,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable;
+}
