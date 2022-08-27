bed8f6d8280abee9b3fbeb953eb534a7a4d3e608
NebullaX <94897257+NebullaX@users.noreply.github.com>
2022-08-27 17:10:40 +0800

    Create SpaceStationV2.sol

---
+++ contracts/SpaceStation/SpaceStationV2.sol
@@ -0,0 +1,207 @@
+pragma solidity 0.7.6;
+
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {EIP712} from "@openzeppelin/contracts/drafts/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import {ISpaceStation} from "../interfaces/ISpaceStation.sol";
+import {IStarNFT} from "../interfaces/IStarNFT.sol";
+
+
+contract SpaceStationV2 is EIP712, ISpaceStation {
+    using Address for address;
+    using SafeMath for uint256;
+
+    /* ============ Events ============ */
+    event EventActivateCampaign(uint256 _cid);
+    event EventClaim(
+        uint256 _cid,
+        uint256 _dummyId,
+        uint256 _nftID,
+        IStarNFT _starNFT,
+        address _sender
+    );
+    event EventClaimCapped(
+        uint256 _cid,
+        uint256 _dummyId,
+        uint256 _nftID,
+        IStarNFT _starNFT,
+        address _sender,
+        uint256 _minted,
+        uint256 _cap
+    );
+    event EventClaimBatch(
+        uint256 _cid,
+        uint256[] _dummyIdArr,
+        uint256[] _nftIDArr,
+        IStarNFT _starNFT,
+        address _sender
+    );
+    event EventClaimBatchCapped(
+        uint256 _cid,
+        uint256[] _dummyIdArr,
+        uint256[] _nftIDArr,
+        IStarNFT _starNFT,
+        address _sender,
+        uint256 _minted,
+        uint256 _cap
+    );
+    event EventForge(
+        uint256 _cid,
+        uint256 _dummyId,
+        uint256 _nftID,
+        IStarNFT _starNFT,
+        address _sender
+    );
+
+    /* ============ Modifiers ============ */
+    /**
+     * Throws if the sender is not a campaign setter, gnosis
+     */
+    modifier onlyCampaignSetter() {
+        _validateOnlyCampaignSetter();
+        _;
+    }
+    /**
+     * Throws if the sender is not a manager: pauser_role and upgrader_role, gnosis
+     */
+    modifier onlyManager() {
+        _validateOnlyManager();
+        _;
+    }
+    /**
+     * Throws if the sender is not a Treasury's manager, gnosis safe
+     */
+    modifier onlyTreasuryManager() {
+        _validateOnlyTreasuryManager();
+        _;
+    }
+    /**
+     * Throws if the contract paused
+     */
+    modifier onlyNoPaused() {
+        _validateOnlyNotPaused();
+        _;
+    }
+
+    /* ============ Enums ================ */
+
+    /* ============ Structs ============ */
+
+    struct CampaignFeeConfig {
+        address erc20; // Address of token asset if required
+        uint256 erc20Fee; // Amount of token if required
+        uint256 platformFee; // Amount of fee for using the service if applicable
+    }
+
+    /* ============ State Variables ============ */
+    // Is contract paused.
+    bool public paused;
+
+    // The galaxy signer(EOA): used to verify EIP-712.
+    address public galaxy_signer;
+    // A campaign setter. TODO: mapping of EOAs to reduce multisig tx gas cost.
+    address public campaign_setter;
+    // The manager which has privilege to do upgrades, pause/unpause.
+    address public manager;
+    // Treasury manager which receives fees.
+    address public treasury_manager;
+
+    // Mapping that stores all fee requirements for a given activated campaign.
+    // If no fee is required at all, should set to all zero values except for `isActive`.
+    mapping(uint256 => CampaignFeeConfig) public campaignFeeConfigs;
+    // hasMinted(dummyID(signature) => bool) that records if the user account has already used the dummyID(signature).
+    mapping(uint256 => bool) public hasMinted;
+    // for capped campaign usage only
+    mapping(uint256 => uint256) public numMinted;
+
+    /* ============ Constructor ============ */
+    constructor(
+        address _galaxy_signer,
+        address _campaign_setter,
+        address _contract_manager,
+        address _treasury_manager
+    ) EIP712("Galaxy", "1.0.0") {
+        // require(galaxy_signer != address(0), "Galaxy signer address must not be null address");
+        // require(campaign_setter != address(0), "Campaign setter address must not be null address");
+        // require(contract_manager != address(0), "Contract manager address must not be null address");
+        // require(treasury_manager != address(0), "Treasury manager address must not be null address");
+        galaxy_signer = _galaxy_signer;
+        campaign_setter = _campaign_setter;
+        manager = _contract_manager;
+        treasury_manager = _treasury_manager;
+    }
+
+    /* ============ External Functions ============ */
+    function activateCampaign(
+        uint256 _cid,
+        uint256 _platformFee,
+        uint256 _erc20Fee,
+        address _erc20
+    ) external onlyCampaignSetter {
+        _setFees(_cid, _platformFee, _erc20Fee, _erc20);
+        emit EventActivateCampaign(_cid);
+    }
+
+    function claim(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256 _dummyId,
+        uint256 _powah,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable override onlyNoPaused {
+        require(!hasMinted[_dummyId], "Already minted");
+        require(
+            _verify(
+                _hash(_cid, _starNFT, _dummyId, _powah, _mintTo),
+                _signature
+            ),
+            "Invalid signature"
+        );
+        hasMinted[_dummyId] = true;
+        _payFees(_cid, 1);
+        uint256 nftID = _starNFT.mint(_mintTo, _powah);
+        emit EventClaim(_cid, _dummyId, nftID, _starNFT, _mintTo);
+    }
+
+    function claimBatch(
+        uint256 _cid,
+        IStarNFT _starNFT,
+        uint256[] calldata _dummyIdArr,
+        uint256[] calldata _powahArr,
+        address _mintTo,
+        bytes calldata _signature
+    ) external payable override onlyNoPaused {
+        require(
+            _dummyIdArr.length > 0,
+            "Array(_dummyIdArr) should not be empty"
+        );
+        require(
+            _powahArr.length == _dummyIdArr.length,
+            "Array(_powahArr) length mismatch"
+        );
+
+        for (uint256 i = 0; i < _dummyIdArr.length; i++) {
+            require(!hasMinted[_dummyIdArr[i]], "Already minted");
+            hasMinted[_dummyIdArr[i]] = true;
+        }
+
+        require(
+            _verify(
+                _hashBatch(_cid, _starNFT, _dummyIdArr, _powahArr, _mintTo),
+                _signature
+            ),
+            "Invalid signature"
+        );
+        _payFees(_cid, _dummyIdArr.length);
+
+        uint256[] memory nftIdArr = _starNFT.mintBatch(
+            _mintTo,
+            _powahArr.length,
+            _powahArr
+        );
+        emit EventClaimBatch(_cid, _dummyIdArr, nftIdArr, _starNFT, _mintTo);
+    }
+}
