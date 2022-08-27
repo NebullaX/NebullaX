27afacfb61f2a9329b5bda6bea87af4b061f258a
NebullaX <94897257+NebullaX@users.noreply.github.com>
2022-08-27 17:08:49 +0800

    Create IStarNFT.sol

---
+++ contracts/interfaces/IStarNFT.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.7.6;
+
+
+interface IStarNFT {
+    /* ============ Events =============== */
+
+    /* ============ Functions ============ */
+
+    function isOwnerOf(address, uint256) external view returns (bool);
+    function getNumMinted() external view returns (uint256);
+    function cid(uint256) external view returns (uint256);
+    // mint
+    function mint(address account, uint256 powah) external returns (uint256);
+    function mintBatch(address account, uint256 amount, uint256[] calldata powahArr) external returns (uint256[] memory);
+    function burn(address account, uint256 id) external;
+    function burnBatch(address account, uint256[] calldata ids) external;
+}
